c4a9e94dbd2f01869dfcb2f03cf26e2636cbe5a5
Zach <zhensler12@gmail.com>
2026-06-13 15:16:38 -0400

    Init

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'preact/hooks'
+import './app.css'
+
+export function App() {
+  return (
+    <main>
+      
+    </main>
+  )
+}
---
+++ src/imageIndex.ts
@@ -0,0 +1,9 @@
+
+interface ImageMetadata {
+    file: string;
+    category: 'Metea'|'Ivy'|'Zeus';
+}
+
+export const imageIndex: ImageMetadata[] = [
+    { file: 'P1011789.JPG', category: 'Metea' },
+]
