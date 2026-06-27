9200263b47525f4ec6e7ad34ee71b281aa9d4161
Zach <zhensler12@gmail.com>
2026-06-27 00:51:32 -0400

    Update VITE config with build location

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: 'https://pages.zach-hensler.com',
+  root: './',
+  build: {
+    outDir: 'build',
+  },
+  publicDir: 'public',
+  test: {
+    exclude: ['node_modules', 'build', '.github'],
+    setupFiles: 'setupTests.ts'
+  }
 })
