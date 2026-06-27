019a836cbe141ecce8f6a49219f57d5afcbc1173
Zach <zhensler12@gmail.com>
2026-06-27 00:14:01 -0400

    Update styles and image set

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,48 @@
-import { useState } from 'preact/hooks'
+import { useEffect, useState, useRef } from 'preact/hooks'
+import {imageIndex} from './imageIndex.ts'
 import './app.css'
 
 export function App() {
+  const [dialogImg, setDialogImg] = useState<string>();
+  const dialogRef = useRef<HTMLDialogElement>();
+
+    useEffect(() => {
+        if (!dialogImg) {
+            dialogRef.current.close();
+        }
+        else {
+            dialogRef.current.showModal();
+        }
+    }, [dialogImg]);
+
+  function Image(image) {
+    function onClick() {
+        setDialogImg(image.file);
+    }
+    return (
+        <img class="image" onClick={onClick} src={"photos/"+image.file} />
+    )
+  }
+
+  function Dialog() {
+      if (!dialogImg) {
+          return <></>;
+      }
+      return (
+          <dialog
+              onClick={() => setDialogImg(null)}
+              ref={dialogRef}>
+              <img className="dialog-image" src={"photos/" + dialogImg}/>
+          </dialog>
+      )
+  }
+
   return (
-    <main>
-      
-    </main>
+    <>
+        <div class="image-container">
+            {imageIndex.map(image => Image(image))}
+        </div>
+        <Dialog />
+    </>
   )
 }
--- src/imageIndex.ts
+++ src/imageIndex.ts
@@ -1,9 +1,18 @@
 
-interface ImageMetadata {
+export interface ImageMetadata {
     file: string;
-    category: 'Metea'|'Ivy'|'Zeus';
 }
 
 export const imageIndex: ImageMetadata[] = [
-    { file: 'P1011789.JPG', category: 'Metea' },
+    { file: 'dappled_path.jpg' },
+    { file: 'mossy_log.jpg' },
+    { file: 'evening_fence.jpg' },
+    { file: 'big_dipper.jpg' },
+    { file: 'white_flower.JPG' },
+    { file: 'evening_pines.JPG' },
+    { file: 'big_red_cloud.jpg' },
+    { file: 'sunset_clouds.jpg' },
+    { file: 'stars.jpg' },
+    { file: 'bird_feeder.JPG' },
+    { file: 'ivy_silhouette.JPG' },
 ]
