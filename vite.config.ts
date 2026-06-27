29e008d1eacfe425c668acf0a13098c7fbc0c3ee
Zach <zhensler12@gmail.com>
2026-06-27 01:01:37 -0400

    Fix URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: 'https://pages.zach-hensler.com',
+  base: 'https://photos.zach-hensler.com',
   root: './',
   build: {
     outDir: 'build',
