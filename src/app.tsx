ae0b8cc20bbf0d86accf98a4d15f2873cd75bad6
Zach <zhensler12@gmail.com>
2026-06-28 13:10:26 -0400

    Add Collections

--- src/app.tsx
+++ src/app.tsx
@@ -1,49 +1,85 @@
-import { useEffect, useState, useRef } from 'preact/hooks'
-import {imageIndex} from './imageIndex.ts'
-import type { ImageMetadata } from './imageIndex.ts'
+import {useEffect, useState, useRef} from 'preact/hooks'
+import {favorites, astro, cats, floral, metea, wildlife} from './imageCollections.ts'
+import type {ImageMetadata} from './imageCollections.ts'
 import './app.css'
 
+type collections = 'Favorites' | 'Astro' | 'Cats' | 'Floral' | 'Metea' | 'Wildlife';
+
 export function App() {
-  const [dialogImg, setDialogImg] = useState<string|null>();
-  const dialogRef = useRef<HTMLDialogElement|null>(null);
+    const [collection, setCollection] = useState<collections>('Favorites')
+    const [dialogImg, setDialogImg] = useState<string | null>();
+    const dialogRef = useRef<HTMLDialogElement | null>(null);
 
     useEffect(() => {
         if (!dialogImg) {
             dialogRef.current?.close();
-        }
-        else {
+        } else {
             dialogRef.current?.showModal();
         }
     }, [dialogImg]);
 
-  function Image(image: ImageMetadata) {
-    function onClick() {
-        setDialogImg(image.file);
+    function Image(image: ImageMetadata) {
+        function onClick() {
+            setDialogImg(image.file);
+        }
+
+        return (
+            <img class="image" onClick={onClick} src={"photos/" + image.file}/>
+        )
     }
+
+    function Dialog() {
+        if (!dialogImg) {
+            return <></>;
+        }
+        return (
+            <dialog
+                onClick={() => setDialogImg(null)}
+                ref={dialogRef}>
+                <img className="dialog-image" src={"photos/" + dialogImg}/>
+            </dialog>
+        )
+    }
+
+    function CollectionImages(): ImageMetadata[] {
+        switch (collection) {
+            case 'Favorites':
+                return favorites;
+            case 'Astro':
+                return astro;
+            case 'Cats':
+                return cats;
+            case 'Floral':
+                return floral;
+            case 'Metea':
+                return metea;
+            case 'Wildlife':
+                return wildlife;
+        }
+    }
+
     return (
-        <img class="image" onClick={onClick} src={"photos/"+image.file} />
+        <>
+            <p className="subtitle">
+                <span>Click to photos expand</span>
+                <div>
+                    <label htmlFor="collection">Collection: </label>
+                    <select
+                        id="collection"
+                        onChange={(e) => setCollection(e.target.value)}>
+                        <option value="Favorites">Favorites</option>
+                        <option value="Metea">Metea</option>
+                        <option value="Astro">Astro</option>
+                        <option value="Cats">Cats</option>
+                        <option value="Floral">Floral</option>
+                        <option value="Wildlife">Wildlife</option>
+                    </select>
+                </div>
+            </p>
+            <div class="image-container">
+                {CollectionImages().map(image => Image(image))}
+            </div>
+            <Dialog/>
+        </>
     )
-  }
-
-  function Dialog() {
-      if (!dialogImg) {
-          return <></>;
-      }
-      return (
-          <dialog
-              onClick={() => setDialogImg(null)}
-              ref={dialogRef}>
-              <img className="dialog-image" src={"photos/" + dialogImg}/>
-          </dialog>
-      )
-  }
-
-  return (
-    <>
-        <div class="image-container">
-            {imageIndex.map(image => Image(image))}
-        </div>
-        <Dialog />
-    </>
-  )
 }
---
+++ src/imageCollections.ts
@@ -0,0 +1,56 @@
+
+export interface ImageMetadata {
+    file: string;
+}
+
+export const favorites: ImageMetadata[] = [
+    { file: 'wildlife/robin.JPG' },
+    { file: 'wildlife/catbird.jpg' },
+    { file: 'metea/dappled_path.jpg' },
+    { file: 'evening_fence.jpg' },
+    { file: 'floral/white_flower.JPG' },
+    { file: 'evening_pines.JPG' },
+    { file: 'astro/big_dipper.jpg' },
+    { file: 'astro/stars.jpg' },
+    { file: 'wildlife/bird_feeder.JPG' },
+    { file: 'cats/ivy_silhouette.JPG' },
+    { file: 'astro/sunset_clouds.jpg' },
+    { file: 'balcony_long_exposure.jpg' }
+]
+
+export const astro: ImageMetadata[] = [
+    { file: 'astro/big_dipper.jpg' },
+    { file: 'astro/stars.jpg' },
+    { file: 'astro/sunset_clouds.jpg' },
+    { file: 'astro/big_red_cloud.jpg' },
+]
+
+export const cats: ImageMetadata[] = [
+    { file: 'cats/smokie_pounce.jpg' },
+    { file: 'cats/zeus_side_lighting.JPG' },
+    { file: 'cats/ivy_silhouette.JPG' },
+    { file: 'cats/luna_sun.jpg' },
+]
+
+export const floral: ImageMetadata[] = [
+    { file: 'floral/white_flower.JPG' },
+    { file: 'metea/white_flower.JPG' },
+    { file: 'colorado_springs/indigo_flower.JPG' },
+    { file: 'colorado_springs/pink_flower.JPG' },
+]
+
+export const metea: ImageMetadata[] = [
+    { file: 'metea/birdhouse.JPG' },
+    { file: 'metea/canopy.JPG' },
+    { file: 'metea/dappled_path.jpg' },
+    { file: 'metea/bridge.JPG' },
+    { file: 'metea/mossy_log.jpg' },
+    { file: 'metea/fallen_tree.jpg' },
+]
+
+export const wildlife: ImageMetadata[] = [
+    { file: 'colorado_springs/mallard.JPG' },
+    { file: 'wildlife/snail.JPG' },
+    { file: 'wildlife/robin.JPG' },
+    { file: 'wildlife/catbird.jpg' },
+]
