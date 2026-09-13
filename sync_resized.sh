0d86775e98aedd89391da487c180dd34881d44f1
Zach <zhensler12@gmail.com>
2026-09-12 20:13:09 -0400

    Add sync support for deleted directories & add rural pics

--- sync_resized.sh
+++ sync_resized.sh
@@ -5,15 +5,19 @@ do
 	PARSED=${dir#./medium/}
 	PARSED=${PARSED%/}
 
-	for image in ./medium/$PARSED/*
-	do
-		FILENAME=${image#./medium/$PARSED/}
+	if [ ! -d full/$PARSED ]; then
+		rm -r medium/$PARSED
+	else
+		for image in ./medium/$PARSED/*
+		do
+			FILENAME=${image#./medium/$PARSED/}
 
-		if [ ! -f full/$PARSED/$FILENAME ]; then
-			echo "File $PARSED/$FILENAME not found in /full, removing from /medium"
-			rm ./medium/$PARSED/$FILENAME
-		fi
-	done
+			if [ ! -f full/$PARSED/$FILENAME ]; then
+				echo "File $PARSED/$FILENAME not found in /full, removing from /medium"
+				rm ./medium/$PARSED/$FILENAME
+			fi
+		done
+	fi
 done
 
 for dir in ./smallest/*/
@@ -21,13 +25,17 @@ do
 	PARSED=${dir#./smallest/}
 	PARSED=${PARSED%/}
 
-	for image in ./smallest/$PARSED/*
-	do
-		FILENAME=${image#./smallest/$PARSED/}
+	if [ ! -d full/$PARSED ]; then
+		rm -r smallest/$PARSED
+	else
+		for image in ./smallest/$PARSED/*
+		do
+			FILENAME=${image#./smallest/$PARSED/}
 
-		if [ ! -f full/$PARSED/$FILENAME ]; then
-			echo "File $PARSED/$FILENAME not found in /full, removing from /smallest"
-			rm ./smallest/$PARSED/$FILENAME
-		fi
-	done
+			if [ ! -f full/$PARSED/$FILENAME ]; then
+				echo "File $PARSED/$FILENAME not found in /full, removing from /smallest"
+				rm ./smallest/$PARSED/$FILENAME
+			fi
+		done
+	fi
 done
